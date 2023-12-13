a0a83cb88dd1639b432d6d7a39eeaa10552a735f
zhubingbing <573082559@qq.com>
2023-12-13 12:23:57 +0800

    improvement: 移除RestTemplate引入

--- src/main/java/com/bosum/gateway/GatewayApplication.java
+++ src/main/java/com/bosum/gateway/GatewayApplication.java
@@ -1,17 +1,12 @@
 package com.bosum.gateway;
 
-import com.bosum.framework.security.utils.RestTemplateUtils;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Import;
-import org.springframework.web.client.RestTemplate;
-
 
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"com.bosum.*"},exclude= {DataSourceAutoConfiguration.class})
-@Import(value = {RestTemplate.class , RestTemplateUtils.class})
 public class GatewayApplication {
 
     public static void main(String[] args) {
