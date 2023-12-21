314b53cb1e1b3d6e4d2eba3d18cf6383ee431ba7
zhubingbing <573082559@qq.com>
2023-12-21 16:42:42 +0800

    improvement: 去除openfeign

--- src/main/java/com/bosum/gateway/GatewayApplication.java
+++ src/main/java/com/bosum/gateway/GatewayApplication.java
@@ -4,11 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @EnableDiscoveryClient
 @SpringBootApplication(scanBasePackages = {"com.bosum.*"},exclude= { DataSourceAutoConfiguration.class})
-@EnableFeignClients(basePackages = {"com.bosum.system.api"})
 public class GatewayApplication {
 
     public static void main(String[] args) {
